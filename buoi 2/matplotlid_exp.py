56e8e593b5251dc0338335dba52fda26e6814dc7
EagleDumb <eagle.nguyen.2304@gmail.com>
2024-05-08 19:53:02 +0700

    matplotlid exp changes

--- buoi 2/matplotlid_exp.py
+++ buoi 2/matplotlid_exp.py
@@ -4,6 +4,6 @@ import numpy as np
 xpoints = np.array([2018,2019,2020,2021,2022])
 ypoints = np.array([10,8,7,9,12])
 
-plt.title("biểu đồ sản lượng khoai mì theo năm", color = "black", backgroundcolor = "yellow")
+plt.title("biểu đồ sản lượng khoai mì theo năm", color = "black", backgroundcolor = "yellow", fontsize = 18)
 plt.plot(xpoints,ypoints)
 plt.show()
