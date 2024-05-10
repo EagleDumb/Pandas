bf344c1996b49f777a8849807a853b7b1cc5b5f2
EagleDumb <eagle.nguyen.2304@gmail.com>
2024-05-10 19:24:36 +0700

    some new commands

--- buoi 2/matplotlid_exp.py
+++ buoi 2/matplotlid_exp.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 
-xpoints = np.array([2018,2019,2020,2021,2022])
-ypoints = np.array([10,8,7,9,12])
+xpoints = np.array([0,2,4,6,8,10])
+ypoints = np.array([0,2000,4000,6000,8000,10000])
 
-plt.title("biểu đồ sản lượng khoai mì theo năm", color = "black", backgroundcolor = "yellow", fontsize = 18)
-plt.plot(xpoints,ypoints)
+plt.title("exp", color = "black", backgroundcolor = "yellow", fontsize = 18)
+plt.plot(xpoints,ypoints, ls = "None")
 plt.show()
